68d3de9adfc21c54e19af5fc28de2ba6f7a63265
silverchris <silverchris@gmail.com>
2023-04-18 17:32:46 -0400

    Cleanup

--- sort.py
+++ sort.py
@@ -2,18 +2,26 @@ import xml.etree.ElementTree as ET
 
 
 def sortchildrenby(parent, attr):
-    parent[:] = sorted(parent, key=lambda child: child.get(attr))
+    try:
+        parent[:] = sorted(parent, key=lambda child: child.get(attr))
+    except TypeError as e:
+        raise(e)
 
+def sort(filename):
+    ET.register_namespace("", "http://kayak.2codeornot2code.org/1.0")
+    parser = ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))
 
-ET.register_namespace("", "http://kayak.2codeornot2code.org/1.0")
-parser = ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))
+    with open(filename, "r") as infile:
+        tree = ET.parse(infile, parser)
+        root = tree.getroot()
+        for bus in root.findall("{http://kayak.2codeornot2code.org/1.0}Bus"):
+            print(bus.tag, bus.attrib)
+            sortchildrenby(bus, "id")
+            for child in bus:
+                if not child.attrib['name']:
+                    child.attrib['name'] = f"Unknown_{child.attrib['id']}"
+                # print(child.tag, child.attrib)
+    tree.write(filename)
 
-with open("skyactive_new.kcd", "r") as infile:
-    tree = ET.parse(infile, parser)
-    root = tree.getroot()
-    for bus in root.findall("{http://kayak.2codeornot2code.org/1.0}Bus"):
-        print(bus.tag, bus.attrib)
-        sortchildrenby(bus, "id")
-        for child in bus:
-            print(child.tag, child.attrib)
-tree.write("skyactive_new.kcd")
+sort("skyactive_new.kcd")
+sort("Vehicle_Settings.kcd")
